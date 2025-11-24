5776df9f5b2c262827b0e83e80bfa9267070458f
Dantte <draaf@cin.ufpe.br>
2025-11-24 14:04:48 -0300

    Adição de features para auxiliar no acompanhamento das alocações em tempo real: check-in, check-out e lista de salas ociosas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
+from datetime import datetime
 
 from app.database import engine, Base, get_db
 from app.models import Sala, Grade, Alocacao
@@ -30,6 +31,9 @@ class NovaDemanda(BaseModel):
     dia_semana: str
     turno: str
     tipo_recurso: str = "EXTRA"
+
+class CheckInRequest(BaseModel):
+    medico_nome: str
 
 @app.get("/")
 def read_root():
@@ -81,10 +85,72 @@ def adicionar_demanda_manual(demanda: NovaDemanda, db: Session = Depends(get_db)
     db.commit()
     return {"message": "Demanda adicionada à grade com sucesso! Rode a alocação novamente para incluí-lo."}
 
+# Rota para realizar CHECK-IN (Ocupar a sala)
+@app.post("/api/salas/{sala_id}/checkin")
+def realizar_checkin(sala_id: str, dados: CheckInRequest, db: Session = Depends(get_db)):
+    # 1. Busca a sala no banco de dados
+    sala = db.query(Sala).filter(Sala.id == sala_id).first()
+
+    if not sala:
+        raise HTTPException(status_code = 404, detail="Sala não encontrada")
+    
+    # 2. Verifica se a sala já está ocupada ou em manutenção
+    if sala.status_atual == "OCUPADA":
+        raise HTTPException(status_code = 400, detail = f"Sala já ocupada por {sala.ocupante_atual}")
+    
+    if sala.is_maintenance:
+        raise HTTPException(status_code = 400, detail = "Esta sala está em manutenção")
+
+    # 3. Atualizar os status das salas aptas a serem ocupadas
+    sala.status_atual = "OCUPADA"
+    sala.ocupante_atual = dados.medico_nome
+    sala.horario_entrada = datetime.now().strftime("%H:%M") # Pega a hora atual do servidor
+
+    db.commit() # Salva no banco de dados
+    db.refresh(sala)
+
+    return {"message": f"Check-in realizado com sucesso para {dados.medico_nome}", "sala": sala}
+
+# Rota para realizar CHECK-OUT(desocupar sala)
+@app.post("/api/salas/{sala_id}/checkout")  
+def realizar_checkout(sala_id: str, db: Session = Depends(get_db)):
+    sala = db.query(Sala).filter(Sala.id == sala_id).first()
+
+    if not sala:
+        raise HTTPException(status_code=404, detail="Sala não encontrada")
+        
+    if sala.status_atual == "LIVRE":
+        return {"message": "Sala já está livre"}
+
+    # Lógica de liberar sala
+    medico_anterior = sala.ocupante_atual
+    sala.status_atual = "LIVRE"
+    sala.ocupante_atual = None
+    sala.horario_entrada = None
+
+    db.commit()
+
+    return {"message": f"Check-out realizado. Sala liberada por {medico_anterior}."}  
+
 # Visualização dos dados
 @app.get("/api/salas")
 def listar_salas(db: Session = Depends(get_db)):
     return db.query(Sala).all()
+
+@app.get("/api/salas/ociosas")
+def listar_salas_ociosas(db: Session = Depends(get_db)):
+    # Retorna apenas as salas que estão LIVRES no momento, ignorando se existia agendamento prévio ou não. Isso resolve o problema da ociosidade
+    salas_livres = db.query(Sala).filter(
+        Sala.status_atual == "LIVRE",
+        Sala.is_maintenance == False
+    ).all()
+
+    return {
+        "total_livres": len(salas_livres),
+        "salas": salas_livres
+    }
+    # Se o médico agendado chegar e encontrar alguém na sala que ele deveria ocupar, ele vai encontrar a mensagem de erro:
+    # Sala ocupada por {residente}. Dessa forma, ele deverá ocupar outra sala utilizando o listar_salas_ociosas
 
 @app.get("/api/grades")
 def listar_demanda(db: Session = Depends(get_db)):
